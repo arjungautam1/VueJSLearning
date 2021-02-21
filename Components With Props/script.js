46a63e3737b7a88efa6dd53724dd230eee6e821d
arjun-sudo <arjungautam5431@gmail.com>
2021-02-21 16:20:00 +0545

    components with props:

---
+++ Components With Props/script.js
@@ -0,0 +1,15 @@
+Vue.component('todo-item',{
+    props:['todo'],
+    template:'<li>{{todo.text}}</li>'
+})
+var app7=new Vue({
+    el:'#app-7',
+    data:{
+        productList:[
+            {id:0,text:'Vegetables'},
+            {id:1,text:'Cheese'},
+            {id:2,text:'Whatever else humnas are supposed to eat'},
+            {id:4,text:'This is fun learning Vue.js'}
+        ]
+    }
+})
