408e5cb02c37664ec2ac0f95df5622a0acb43333
arjungautam1 <arjun@moco.com.np>
2021-02-21 12:09:48 +0545

    vue js

---
+++ Basic/index.js
@@ -0,0 +1,6 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: "Hello This is Arjun using Vue JS for the first time ."
+    }
+})
---
+++ Basic/reactive.js
@@ -0,0 +1,6 @@
+var app2 = new Vue({
+    el: '#app-2',
+    data: {
+        message: 'You loaded the page ' + new Date().toLocaleString()
+    }
+})
